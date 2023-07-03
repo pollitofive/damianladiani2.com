c01929bb8df6a76134685635e82861d615b09c26
Damian <damianladiani@gmail.com>
2023-07-03 19:13:16 -0300

    I include the html, css, js, fonts, etc. I also separe the code in files

---
+++ resources/views/content/home.blade.php
@@ -0,0 +1,24 @@
+<!-- Home Subpage -->
+<section data-id="home" class="animated-section start-page">
+    <div class="section-content vcentered">
+
+        <div class="row">
+            <div class="col-sm-12 col-md-12 col-lg-12">
+                <div class="title-block">
+                    <h2>Damián Ladiani</h2>
+                    <div class="owl-carousel text-rotation">
+                        <div class="item">
+                            <div class="sp-subtitle">Laravel Developer</div>
+                        </div>
+
+                        <div class="item">
+                            <div class="sp-subtitle">PHP Developer</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+<!-- End of Home Subpage -->
