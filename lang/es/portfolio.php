b98d8227284b5d6826b772a70fd4d165fa330c64
Damian Ladiani <damianladiani@gmail.com>
2023-07-27 22:59:03 +0000

    I update the url of the old resume in my hosting

--- lang/es/portfolio.php
+++ lang/es/portfolio.php
@@ -6,7 +6,7 @@ return [
     'projects' => [
         ['name' => 'Wadaboo','photo' => 'img/portfolio/wadaboo1.jpg', 'url' => 'https://wadaboo.damianladiani.com/','code' => 'https://github.com/pollitofive/wadaboo'],
         ['name' => 'Wadapay','photo' => 'img/portfolio/wadapay1.jpg', 'url' => 'https://wadapay.damianladiani.com/','code' => 'https://github.com/pollitofive/wadapay'],
-        ['name' => 'damianladiani.com','photo' => 'img/portfolio/damianladiani.com1.jpg', 'url' => 'https://damianladiani.com','code' => 'https://github.com/pollitofive/damianladiani.com'],
+        ['name' => 'damianladiani.com','photo' => 'img/portfolio/damianladiani.com1.jpg', 'url' => 'https://resume.damianladiani.com','code' => 'https://github.com/pollitofive/damianladiani.com'],
         ['name' => 'skills','photo' => 'img/portfolio/skills1.jpg', 'url' => 'https://skills.damianladiani.com/','code' => 'https://github.com/pollitofive/skills'],
         ['name' => 'cryptolivewire','photo' => 'img/portfolio/cryptolivewire1.jpg', 'url' => 'https://cryptolivewire.damianladiani.com/','code' => 'https://github.com/pollitofive/crypto-livewire'],
         ['name' => 'damianladiani2.com','photo' => 'img/portfolio/damianladiani2.com1.jpg', 'url' => 'https://damianladiani.com','code' => 'https://github.com/pollitofive/damianladiani2.com'],
